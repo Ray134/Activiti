14f7cf5dd999d5eb233c97517f9c40842675ed92
Ray123 <myrevenant@126.com>
2021-10-09 15:41:01 +0800

    change the project to spring boot version based on 1.2.6

--- modules/activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/JacksonConfiguration.java
+++ modules/activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/JacksonConfiguration.java
@@ -10,28 +10,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.activiti.app.conf;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-/**
- * @author Joram Barrez
- */
-@Configuration
-public class JacksonConfiguration {
-
-    @Bean()
-    public ObjectMapper objectMapper() {
-        
-    	// To avoid instantiating and configuring the mapper everywhere
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        
-        return mapper;
-    }
-
-}
+//package org.activiti.app.conf;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+//
+//*
+// * @author Joram Barrez
+//
+//@Configuration
+//public class JacksonConfiguration {
+//
+//    @Bean()
+//    public ObjectMapper objectMapper() {
+//
+//    	// To avoid instantiating and configuring the mapper everywhere
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+//
+//        return mapper;
+//    }
+//
+//}
